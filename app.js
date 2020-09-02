4c4f3faf7ae02d212a76db76a793efe9e2ec4b80
Pablo Marino <pmarino2013@gmail.com>
2020-09-02 20:56:36 -0300

    Clase sobre clases

---
+++ app.js
@@ -0,0 +1,11 @@
+/* 
+Escribir una clase Producto para crear objetos
+estos objetos tendran propiedades:
+codigo
+nombre 
+precio 
+metodo: imprimirDatos 
+crear tres instancias de la clase
+las vamos a guardar en un Array
+usar el metodo imprimirDatos para mostrar los valores de los tres objetos 
+*/
